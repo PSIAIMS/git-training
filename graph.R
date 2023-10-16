4fcaa0b5d421f79e83def045d6090e1e6ef526d5
Christina Fillmore <55274484+statasaurus@users.noreply.github.com>
2023-10-16 11:22:34 +0100

    Code V4

--- graph.R
+++ graph.R
@@ -1,4 +1,4 @@
-# LOOKING AT JOURNAL B
+# FINALISING JOURNAL B
 library(tidyverse)
 
 surv<-read.csv("data/whas500.csv")%>%
@@ -8,7 +8,7 @@ surv<-read.csv("data/whas500.csv")%>%
          CVD_C = ifelse(CVD==1, "Yes", "No")) 
 
 
-pubgraph1<-
+pubgraph1 <-
   ggplot(data = surv, mapping = aes(x =DIASBP , y =SYSBP )) + 
   geom_point() + 
   facet_grid(rows = vars(AFB_C), cols = vars(GENDER_C)) +
@@ -16,9 +16,17 @@ pubgraph1<-
        y="Systolic Blood Pressure",
        x="Diastolic Blood Pressure",
        caption="Data source: Survival 500 data")+
-  geom_smooth(mapping=aes(group=NA,  x=DIASBP,y=SYSBP))
+  geom_smooth(mapping=aes(group=NA,  x=DIASBP,y=SYSBP))+
+  theme(panel.spacing = unit(0.5,"cm",data=NULL),
+        strip.text.x=element_text(size=12, color="blue",face="bold.italic"),
+        strip.text.y=element_text(size=12, color="blue",face="bold"),
+        plot.title=element_text(face="italic", color="Blue"),
+        panel.grid.minor=element_line(linetype="solid",color="black"),
+        plot.background=element_rect(fill="lightblue",color="black")
+  )
 
-ggplot2::ggsave(filename="images/pubgraph1-dot3.jpg",
+
+ggplot2::ggsave(filename="images/pubgraph1-dot4.jpg",
                 plot=pubgraph1,
                 device="jpg",
                 dpi = 72,
